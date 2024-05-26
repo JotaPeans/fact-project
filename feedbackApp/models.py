ed9bab1d084ad49100c1c4f78cc9757e9386a8ab
JotaPeans <jpfontesferreira1@gmail.com>
2024-05-26 02:07:54 -0300

    fact model

--- feedbackApp/models.py
+++ feedbackApp/models.py
@@ -4,15 +4,11 @@ from django.contrib.auth.models import User
 class Aluno(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    pensamento_critico_criatividade = models.FloatField(verbose_name="Pensamento Crítico e Criatividade")
-    comunicacao = models.FloatField(verbose_name="Comunicação")
-    colaboracao = models.FloatField(verbose_name="Colaboração")
-    qualidade_entregas = models.FloatField(verbose_name="Qualidade das Entregas")
-    presenca = models.FloatField(verbose_name="Presença")
-    entrega_prazos = models.FloatField(verbose_name="Entrega e Prazos")
+    
 
     def __str__(self) -> str:
         return self.nome
+    
 
 class Grupo(models.Model):
     nome = models.CharField(max_length=200)  
@@ -21,3 +17,19 @@ class Grupo(models.Model):
 
     def __str__(self) -> str:
         return self.nome
+
+
+class Fact(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+    nome = models.CharField(max_length=255, default="Status Report 1")
+    pensamento_critico_criatividade = models.FloatField(verbose_name="Pensamento Crítico e Criatividade")
+    comunicacao = models.FloatField(verbose_name="Comunicação")
+    colaboracao = models.FloatField(verbose_name="Colaboração")
+    qualidade_entregas = models.FloatField(verbose_name="Qualidade das Entregas")
+    presenca = models.FloatField(verbose_name="Presença")
+    entrega_prazos = models.FloatField(verbose_name="Entrega e Prazos")
+
+    def __str__(self) -> str:
+        return self.nome + " - " + self.aluno.nome
