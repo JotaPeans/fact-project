c00dbf2215c2fc48af4ee0c494fee30a242801d7
Tanaka <130470569+helotanaka@users.noreply.github.com>
2024-06-07 17:44:08 -0300

    Teste adicionar admin

--- cypress/e2e/testeAdicionarAdmin.cy.js
+++ cypress/e2e/testeAdicionarAdmin.cy.js
@@ -10,12 +10,24 @@ describe('Adicionar admin a um grupo', () => {
       cy.get('#name').type("Grupo 1")
       cy.get('form > button').contains('Enviar').click()
 
+      cy.visit('/')
+
       cy.get('.group > a').contains('Grupo 1').closest('.group').as('G1') //captura elemento pai
       cy.get('@G1').within(() => { //define um contexto restrito ao elemento pai capturado
           cy.get('.popover-button').click()
           cy.get('.popover-content > .gerenciar-group').click()
       })
-      cy.get('#usuariosProfessores').cy.select()
+      cy.get('#usuariosProfessores').select('coordenador')
+      cy.get('#botaoDoConfia').click()
+      cy.get('#gerenciarGroupModal > div > .close-modal').click()
+
+      cy.visit('/')
+
+      cy.get('@G1').within(() => { //define um contexto restrito ao elemento pai capturado
+          cy.get('.popover-button').click()
+          cy.get('.popover-content > .delete-group').click()
+      })
+      cy.get('#delete-group-link').click()
   })
  
 })
